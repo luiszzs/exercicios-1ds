e558dad5e21d2c0bf13f2749b5734a7d020627c3
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-10-14 10:26:43 -0300

    Update index.js

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
-
+let resultado = 1
+while (resultado <= 10){
+    console.log(resultado)
+    resultado += 1
+}
+resultado -= 1
+console.log(`O seu resultado é: ${resultado}`)
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
