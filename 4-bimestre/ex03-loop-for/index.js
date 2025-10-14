45329310c5a23d96b0805a41ef13471318d4a03d
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-10-14 14:29:25 +0000

    feito!

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
-
+let inicio = 0
+for(let i = inicio; i <= 10; i++  ){
+    inicio = inicio + i
+    console.log(inicio)
+}
+let resultado = inicio
+console.log(`Sua soma é: ${resultado}`)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
