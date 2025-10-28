33e8b1c7d177b77a824f09efea7205d07a70cbe3
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-10-28 20:43:44 +0000

    feito

--- 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
+++ 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
@@ -1,1 +1,46 @@
 // CRIE UMA LÓGICA ABAIXO
+const prompt = require("prompt-sync")()
+
+console.log("Bem-vindo a TechFactory")
+console.log("Aqui nós realizamos um relatório completo da produção diária")
+let dias = 0 
+let producao = []
+let total = 0
+
+dias++
+let quantosDias = (parseInt(prompt("Quantos dias deseja simular? ")))
+for(let i = 0; i < quantosDias; i++){
+    producao.push(parseInt(prompt(`Digite a quantidade produzida no dia ${i + 1}:`)))
+}
+for(let inicio = 0; inicio < producao.length; inicio++ ){
+    total += producao[inicio]
+}
+ 
+let maior = producao[0]
+let menor = producao[0]
+for(let maiorOuMenor = 1; maiorOuMenor < producao.length; maiorOuMenor++){
+    if(producao[maiorOuMenor] > maior){
+        maior = producao[maiorOuMenor]
+    }
+    if(producao[maiorOuMenor] < menor){
+        menor = producao[maiorOuMenor]
+    }
+}
+
+ let mediaDeProducao = total / producao.length
+
+console.log(`
+--- Relatórioo de produção ---
+Produções registradas: ${producao}
+Total produzido: ${total}
+Média diária: ${mediaDeProducao}
+Maior produção: Maior produção: ${maior} ( dia ${producao.indexOf(maior) + 1} )
+Menor produção: Menor produção: ${menor} ( dia ${producao.indexOf(menor) + 1} )
+Ordem crescente: ${producao.sort((a, b) => a - b)}
+Ordem decrescente: ${producao.sort((a, b) => b - a)}
+    `)
+    if( mediaDeProducao >= total / 2){
+        console.log("Produção estável!")
+    }else{
+        console.log("Produção abaixo do ideal.")
+    }
