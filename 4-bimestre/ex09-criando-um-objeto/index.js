e86addcd97a26fd39252386f01d7ffb8d134fd30
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-10-30 17:33:18 +0000

    feito!

--- 4-bimestre/ex09-criando-um-objeto/index.js
+++ 4-bimestre/ex09-criando-um-objeto/index.js
@@ -1,4 +1,11 @@
 // CRIE UMA LÓGICA AQUI
+let produto = {
+    nome: "Teclado",
+    preco: 120,
+    estoque: 25,
+    categoria: "Periféricos"
+}
 
+console.log(produto)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { produto }
--- 4-bimestre/ex10-acessando-e-alterando-obj/index.js
+++ 4-bimestre/ex10-acessando-e-alterando-obj/index.js
@@ -1,4 +1,14 @@
 // CRIE UMA LÓGICA AQUI
+let carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2020,
+    cor: "Prata"
+}
+console.log(carro.modelo)
 
+carro.cor = "Preto"
+carro.preco = 120000
+console.log(carro)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
-module.exports = { produto }
+module.exports = { carro }
