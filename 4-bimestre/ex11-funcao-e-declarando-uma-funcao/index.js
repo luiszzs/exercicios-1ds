ebd160a334cdfc7f73d29995a857a557eda1a471
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-11-06 17:54:43 +0000

    terminar-depois

--- 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
+++ 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
@@ -1,4 +1,16 @@
 // CRIE UMA LÓGICA AQUI
+function boasVindas(){
+    return "Bem-vindo(a) ao sistema!"
+}
 
+function soma (a, b){
+    return a + b
+}
+
+let bemVindo = boasVindas()
+let resultado = soma(5, 3)
+
+console.log(bemVindo)
+console.log(`Resultado da soma: ${resultado}`)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { boasVindas, soma }
