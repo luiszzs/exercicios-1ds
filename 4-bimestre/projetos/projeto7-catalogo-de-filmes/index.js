ff039e7fb7b60ab5c608f190a43c37f91d0dbd7d
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-11-11 13:36:25 +0000

    feito!

--- 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
+++ 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
@@ -1,50 +1,77 @@
 // CRIE UMA LÓGICA ABAIXO
-let prompt = require("prompt-sync")()
-
-let filme1 = {}
-let filme2 = {}
-let filme3 = {}
-let filme4 = {}
-let filme5 = {}
-let filme6 = {}
-let filme7 = {}
-let filme8 = {}
-let filme9 = {}
-let filme10 = {}
+let filme1 = {
+    titulo: "Batman: Beggins",
+    genero: "Ação/Crime",
+    ano: 2008,
+    avaliacao: 8.5
+}
+let filme2 = {
+    titulo: " Batman: O Cavaleiro Das Trevas",
+    genero: "Ação/Crime",
+    ano: 2009,
+    avaliacao: 9.5
+}
+let filme3 = {
+    titulo: "Batman: Cavaleiro Das Trevas Ressurge",
+    genero: "Ação/Crime",
+    ano: 2011,
+    avaliacao: 8.0
+}
+let filme4 = {
+    titulo: "Batman: Cavaleiro Das Trevas Part. 1 (Animação)",
+    genero: "Ação/Crime",
+    ano: 2016 ,
+    avaliacao: 10.0
+}
+let filme5 = {
+    titulo: "Batman: Cavaleiro Das Trevas Part. 2 (Animação)",
+    genero: "Ação/Crime",
+    ano: 2017,
+    avaliacao: 10.0
+}
+let filme6 = {
+    titulo: "Batman e o Longo Dia Das Bruxas Part. 1",
+    genero: "Ação/Crime",
+    ano: 2018,
+    avaliacao: 9.7
+}
+let filme7 = {
+    titulo: "Batman e o Longo Dia Das Bruxas Part. 2",
+    genero: "Ação/Crime",
+    ano: 2019,
+    avaliacao: 9.8
+}
+let filme8 = {
+    titulo: "Batman: Piada Mortal",
+    genero: "Ação/Crime",
+    ano: 2019,
+    avaliacao: 8.9
+}
+let filme9 = {
+    titulo: "The Batman Part. I",
+    genero: "",
+    ano: 2022,
+    avaliacao: 10.0
+}
+let filme10 = {
+    titulo: "Batman: Sangue em Família",
+    genero: "",
+    ano: 2020,
+    avaliacao: 9.3
+}
 
 let filme = [filme1, filme2, filme3, filme4, filme5, filme6, filme7, filme8, filme9, filme10]
-
-for(let i = 0; i < filme.length; i++){
-
-  let titulo = prompt(`Digite o título do ${i + 1}°filme: `)
-  let genero = prompt(`Digite o gênero do filme: `)
-  let ano = parseInt(prompt(`Digite o ano em que o filme foi lançado: `))
-  let avaliacao = parseFloat(prompt(`Digite a avaliação do filme: `))
- 
-    while (avaliacao > 10 || avaliacao < 0) {
-    console.log("Avalie apenas de 0 a 10")
-    avaliacao = parseFloat(prompt("Digite a avaliação do filme: "))
-}
-}
-filme[i].titulo = titulo
-filme[i].genero = genero
-filme[i].ano = ano
-filme[i].avaliacao = avaliacao
 
 let avaliacaoMaior = filme[0]
 let soma = 0 
 for(let i = 0; i < filme.length; i++){
     soma += filme[i].avaliacao
-    
-    if(filme[i].avaliacao > avaliacaoMaior){
-        filme[i].avaliacao = avaliacaoMaior
-    }
 }
-
 for(let i = 0; i < filme.length; i++){
     console.log(i + 1, filme[i].titulo )
 }
-console.log("Filme mais bem avaliado:" + avaliacaoMaior)
-console.log(`Média geral das avaliações: ${soma / filme.length}`)
+let media = soma / filme.length
+console.log(`Filme mais bem avaliado:` )
+console.log(`Média geral das avaliações: ${media.toFixed(2)}`)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { filme1, filme2, filme3, filme4, filme5, filme6, filme7, filme8, filme9, filme10 }
